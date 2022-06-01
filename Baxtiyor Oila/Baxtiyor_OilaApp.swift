e41a606f5ce585ec7f0e1b520a25a0ce6cdad70b
SAMANDAR <98484608+AVATAR0704@users.noreply.github.com>
2022-06-01 22:01:25 +0500

    Initial Commit

---
+++ Baxtiyor Oila/Baxtiyor_OilaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Baxtiyor_OilaApp.swift
+//  Baxtiyor Oila
+//
+//  Created by Macservis on 21/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Baxtiyor_OilaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Baxtiyor Oila/Screens/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Baxtiyor Oila
+//
+//  Created by Macservis on 21/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+  
+        viewsomeView
+        
+    }
+    
+    var viewsomeView: some View {
+        
+        NavigationView {
+            
+            Text("Hello Samandar")
+            
+            
+            
+            
+        }
+        
+        
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
